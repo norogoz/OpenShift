c1e7e3671f4d7c0423e018f45e68590fcb0d4105
Grzegorz Rogoziński <norogoz@gmail.com>
2019-06-07 18:16:13 +0200

    Added Javadoc descriptions

--- src/main/java/docker/AppConfig.java
+++ src/main/java/docker/AppConfig.java
@@ -8,16 +8,22 @@ import javax.ws.rs.core.Application;
 
 //host:8080/docker/calculate
 
+/**
+ * Manages further web resources.
+ * 
+ * @author Grzegorz Norbert Rogozinski
+ *
+ */
+
 @ApplicationPath("calculate")
 public class AppConfig extends Application {
-	private Set<Class<?>> resources=new HashSet<>();
-	
-	public AppConfig()
-	{
+	private Set<Class<?>> resources = new HashSet<>();
+
+	public AppConfig() {
 		System.out.println("Created AppConfig");
 		resources.add(Calculation.class);
 	}
-	
+
 	@Override
 	public Set<Class<?>> getClasses() {
 		return resources;
--- src/main/java/docker/Calculation.java
+++ src/main/java/docker/Calculation.java
@@ -12,8 +12,22 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+/**
+ * Handles HTML GET and POST requests sent to url
+ * http://host:8080/docker/calculation/.
+ * 
+ * @author Grzegorz Norbert Rogozinski
+ *
+ */
 @Path("")
 public class Calculation {
+
+	/**
+	 * Responds to GET request.
+	 * 
+	 * @param e expression to calculate.
+	 * @return value of the expression as a String.
+	 */
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("{e}")
@@ -22,6 +36,12 @@ public class Calculation {
 		return result;
 	}
 
+	/**
+	 * Responds to POST request.
+	 * 
+	 * @param e expression to calculate passed in HTML body.
+	 * @return value of the expression as a String.
+	 */
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("post")
@@ -32,14 +52,14 @@ public class Calculation {
 
 	private String calculate(String expression) {
 		String value = "";
-		ScriptEngineManager mgr = new ScriptEngineManager();
-		ScriptEngine engine = mgr.getEngineByName("JavaScript");
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
 
 		try {
 			value = engine.eval(expression).toString();
 		} catch (ScriptException e1) {
 			e1.printStackTrace();
 			System.out.println(expression);
+			return "NaN";
 		}
 		return value;
 	}
